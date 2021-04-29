b40f78a5a2e116681d56c61e992ed3dfcc2cbe4e
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-04-28 22:14:49 -0500

    model and some associations

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ecommercebackend
+# E-Commerce Back End
+
--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: 'product_tag',
+    unique: false
+  },
+  as: 'product_descrip'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: 'product_tag',
+    unique: false
+  },
+  as: ''
+})
 
 module.exports = {
   Product,
